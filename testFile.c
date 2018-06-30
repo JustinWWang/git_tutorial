21f7d3029a79b80035986d09686c0f870689dd4a
jtwwang <justin.w.wang.0@gmail.com>
2018-06-30 13:51:53 -0700

    Made some changes

--- testFile.c
+++ testFile.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	printf("This is the master branch");
-	printf("This is the initial print message!\n");
+	printf("We made changes after stashing!\n");
 	return 0;
 }
 
